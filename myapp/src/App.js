f5e986911bae0961cebefae489cbe794b0a96460
u2017113 <73095355+u2017113@users.noreply.github.com>
2020-11-13 10:48:28 +0500

    Update App.js

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit faizan
+          Edit hammad sab
         </p>
         <a
           className="App-link"
